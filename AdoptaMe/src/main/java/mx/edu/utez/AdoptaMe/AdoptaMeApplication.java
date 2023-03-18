c97e354a118e5b3803ba4a65cc5b718348142b4c
Tania Belen Galindo Villegas <20203tn155@utez.edu.mx>
2023-03-18 14:01:49 -0600

    AdoptaMe project created

---
+++ AdoptaMe/src/main/java/mx/edu/utez/AdoptaMe/AdoptaMeApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.AdoptaMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptaMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaMeApplication.class, args);
+	}
+
+}
