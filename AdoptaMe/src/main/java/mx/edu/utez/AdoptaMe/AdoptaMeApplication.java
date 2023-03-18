a36a61382124f518b8dd4231deb96a419de89f0f
Tania Belen Galindo Villegas <20203tn155@utez.edu.mx>
2023-03-18 14:51:04 -0600

    AdoptameAplication updated

--- AdoptaMe/src/main/java/mx/edu/utez/AdoptaMe/AdoptaMeApplication.java
+++ AdoptaMe/src/main/java/mx/edu/utez/AdoptaMe/AdoptaMeApplication.java
@@ -1,11 +1,12 @@
 package mx.edu.utez.AdoptaMe;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class AdoptaMeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdoptaMeApplication.class, args);
 	}
