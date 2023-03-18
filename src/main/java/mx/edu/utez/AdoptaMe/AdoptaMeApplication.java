ffee4ca618b54d5b3c0b5a2c9c41cef241685e4d
Daniel Marquina <saiko.daniel1401gmail.com>
2023-03-18 14:59:10 -0600

    dani

---
+++ src/main/java/mx/edu/utez/AdoptaMe/AdoptaMeApplication.java
@@ -0,0 +1,15 @@
+package mx.edu.utez.AdoptaMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class AdoptaMeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptaMeApplication.class, args);
+		
+	}
+
+}
